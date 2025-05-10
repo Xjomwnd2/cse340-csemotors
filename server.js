ed75decdaf45886535602e5764c25652a2607f37
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-10 19:27:35 +0300

    COMMIT

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
+app.use(express.static("public"));
 app.use(static)
 // index route
 app.get("/", function(req, res){
