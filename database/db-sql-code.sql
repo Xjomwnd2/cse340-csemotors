f63e0cb33f16c4c70ff0f7613b0f7a197511bcc7
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-12 02:38:53 +0300

    COMMIT

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -32,3 +32,10 @@ inv_color character varying NOT NULL,
 classification_id integer NOT NULL,
 CONSTRAINT inventory_pkey PRIMARY KEY (inv_id)
 );
+
+-- Create relationship between 'classification' and 'inventory' tables
+ALTER TABLE IF EXISTS public.inventory
+    ADD CONSTRAINT fk_classification FOREIGN KEY (classification_id)
+	REFERENCES public.classification (classification_id) MATCH SIMPLE
+	ON UPDATE CASCADE
+	ON DELETE NO ACTION;
