ea2f60163259f52241b19abdf23ff3a6d2037006
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-24 01:13:30 +0300

    COMMIT

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,7 +1,11 @@
 // Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/invController")
+const express = require("express");
+const router = new express.Router(); 
+const invController = require("../controllers/invController");
+const utilities = require("../utilities/");
+
+// Route for specific vehicle detail view
+router.get("/detail/:invId", utilities.handleErrors(invController.buildVehicleDetail));
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
