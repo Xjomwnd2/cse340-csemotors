c8060cf3cb8bb6aa91cd2e7fc7b1c0cdc5f55f79
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-24 01:34:07 +0300

    COMMIT

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,12 +1,12 @@
-const utilities = require("../utilities/")
-const baseController = {}
+const utilities = require("../utilities/");
+const baseController = {};
 
 baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
-}
+  const nav = await utilities.getNav();
+  res.render("index", {title: "Home", nav});
+};
 
 // const nav = await getNav();
-utilities.handleErrors(baseController.buildHome)
+utilities.handleErrors(baseController.buildHome);
 
-module.exports = baseController
+module.exports = baseController;
