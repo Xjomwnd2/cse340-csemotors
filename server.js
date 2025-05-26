a966c5a4cd832753ea5b836c1e5c1c99cc651fdd
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-27 00:34:10 +0300

    commit

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const utilities = require("./utilities/");
 const session = require("express-session");
 const pool = require('./database/');
 const accountRoute = require("./routes/accountRoute");
+const session = require('express-session');
 
 
 /* ****************************************
