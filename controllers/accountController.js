ce880a914e8ec4f8a46354abf1766485b9bba3a8
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-26 04:57:10 +0300

    commit

---
+++ controllers/accountController.js
@@ -0,0 +1,12 @@
+// controllers/accountController.js
+
+const accountController = {
+  login: (req, res) => {
+    res.send("Login handler not implemented yet.");
+  },
+  register: (req, res) => {
+    res.send("Register handler not implemented yet.");
+  }
+};
+
+module.exports = accountController;
