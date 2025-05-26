04ce9a0d34b91460c0edfa7d538af69a87a7bd62
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-26 05:14:19 +0300

    COMMIT

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ const pool = require('./database/');
 const accountRoute = require("./routes/accountRoute");
 
 
+/* ****************************************
+*  Deliver login view
+* *************************************** */
+async function buildLogin(req, res, next) {
+  let nav = await utilities.getNav();
+  res.render("account/login", {
+    title: "Login",
+    nav,
+  });
+}
+
+module.exports = { buildLogin };
+
 /* ***********************
  * Middleware
  * ************************/
