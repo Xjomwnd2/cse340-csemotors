08704a90800b50705a78b9a80a28d18dc3347f98
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-30 02:34:02 +0300

    COMMIT

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -29,6 +29,18 @@ async function getInventoryByClassificationId(classification_id) {
   }
 };
 
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email){
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1"
+    const email = await pool.query(sql, [account_email])
+    return email.rowCount
+  } catch (error) {
+    return error.message
+  }
+}
 
 // Export the functions so they can be used elsewhere
-module.exports = {getClassifications, getInventoryByClassificationId};
+module.exports = {getClassifications, getInventoryByClassificationId, checkExistingEmail};
