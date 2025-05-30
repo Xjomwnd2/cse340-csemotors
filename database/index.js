48f8f9bfc3555fdf033f98f67d1fda888954878d
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-31 01:05:06 +0300

    commit

--- database/index.js
+++ database/index.js
@@ -1,6 +1,15 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
+
+
 let pool;
 
 if (process.env.NODE_ENV === "development") {
