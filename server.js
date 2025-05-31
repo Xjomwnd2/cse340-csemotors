915881cecf3baad246e3cd900abffd33de6573b9
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-31 03:41:59 +0000

    May 31, 2025, 6:41 AM

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ const session = require("express-session");
 const pool = require('./database/');
 const accountRoute = require("./routes/accountRoute");
 
-
+const pool = new Pool({
+  user: process.env.DB_USER || 'cse340',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'your_database_name',
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
 /* ****************************************
 *  Deliver login view
 * *************************************** */
