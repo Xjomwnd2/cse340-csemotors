54d2b19d95cb435aed4891bd3a82c6f731f73e3e
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-31 03:43:46 +0000

    May 31, 2025, 6:43 AM

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
 const utilities = require("./utilities/");
 const session = require("express-session");
+const pool = require('./database/');
 const accountRoute = require("./routes/accountRoute");
 
 const pool = new Pool({
