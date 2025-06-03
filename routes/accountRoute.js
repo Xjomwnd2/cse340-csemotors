420a183c7cb0d46e7e55614fc166fbcb0735ef5e
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-03 03:17:57 +0300

    COMMIT

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,40 +3,30 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
-const { registrationRules } = require("../utilities/account-validation");
 const regValidate = require('../utilities/account-validation'); // Adjust path if needed
 
-
-
-// Route to build "My Account" view
+// Route to build "My Account" view (make sure buildAccount exists in your controller)
+// If you don't have it yet, comment this out or implement it!
 router.get(
   "/", 
-  utilities.handleErrors(accountController.buildAccount)
+  utilities.handleErrors(accountController.buildAccount) // <-- Only if this exists!
 );
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 // Route to deliver the registration view
-router.get("/register", accountController.buildRegister);
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Route to process the registration form submission
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+router.post('/register', utilities.handleErrors(accountController.register));
 
-// Process the login request
+// Process the login request with validation middleware
 router.post(
   "/login",
   regValidate.loginRules(),
   regValidate.checkLoginData,
-  utilities.handleErrors(accountController.accountLogin)
-)
-
-// Process the login attempt
-router.post(
-  "/login",
-  (req, res) => {
-    res.status(200).send('login process')
-  }
-)
+  utilities.handleErrors(accountController.login)
+);
 
 module.exports = router;
