861a6f4b9c1a5a3b3148e0374bb58935ea5537d4
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-03 04:08:48 +0300

    COMMIT

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,37 @@
+function buildInventoryList(data) {
+  let inventoryDisplay = document.getElementById("inventoryDisplay");
+
+  // Build the header row
+  let table = `<thead>
+    <tr>
+      <th>Vehicle Name</th>
+      <th>Model</th>
+      <th>Year</th>
+      <th>Price</th>
+      <th>Modify</th>
+      <th>Delete</th>
+    </tr>
+  </thead>`;
+
+  // Build each row from the inventory data
+  table += "<tbody>";
+  data.forEach(function (vehicle) {
+    table += `<tr>
+      <td>${vehicle.inv_make} ${vehicle.inv_model}</td>
+      <td>${vehicle.inv_model}</td>
+      <td>${vehicle.inv_year}</td>
+      <td>$${vehicle.inv_price.toLocaleString()}</td>
+      <td><a href='/inv/edit/${vehicle.inv_id}' title='Click to update'>Modify</a></td>
+      <td><a href='/inv/delete/${vehicle.inv_id}' title='Click to delete'>Delete</a></td>
+    </tr>`;
+  });
+  table += "</tbody>";
+
+  // Inject the table into the page
+  inventoryDisplay.innerHTML = table;
+}
+
+
 'use strict'
 
 // Get a list of items in inventory based on the classification_id 
