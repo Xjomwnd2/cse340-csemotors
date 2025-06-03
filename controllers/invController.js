a65c263cd556573ec4ee0e60d8944cd9e0e58471
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-03 04:30:57 +0300

    COMMIT

--- controllers/invController.js
+++ controllers/invController.js
@@ -21,3 +21,15 @@ invCont.buildByClassificationId = async function (req, res, next) {
 };
 
   module.exports = invCont;
+
+  /* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
