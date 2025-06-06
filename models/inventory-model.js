ef626e050e32c492721e971a54521255b2c9aafc
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-06 07:36:20 +0300

    commit

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -48,11 +48,11 @@ async function checkExistingEmail(account_email){
 *********************************************************** */
 async function deleteInventoryItem(inv_id) {
   try {
-    const sql = "DELETE FROM inventory WHERE inv_id = $1"
-    const data = await pool.query(sql, [inv_id])
-    return data
+    const sql = "DELETE FROM inventory WHERE inv_id = $1";
+    const data = await pool.query(sql, [inv_id]);
+    return data;
   } catch (error) {
-  new Error("Delete Inventory Error")
+  new Error("Delete Inventory Error"); // Add 'throw' so the error propagates
   }
 }
 // Export the functions so they can be used elsewhere
