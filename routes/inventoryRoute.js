99f6834ba17d744919b883e1a6b0d9803bae53c5
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-06 07:49:31 +0300

    commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = new express.Router(); 
 const invController = require("../controllers/invController");
+const utilities = require("../utilities/index");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -32,5 +33,27 @@ router.post("/delete", utilities.handleErrors(async (req, res, next) => {
     next(error);
   }
 }));
+
+// Route to display delete confirmation view
+router.get("/delete/:inv_id", async (req, res, next) => {
+  try {
+    const inv_id = parseInt(req.params.inv_id)
+    
+    // Call controller function to handle delete confirmation view
+    await invController.buildDeleteView(req, res, next)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Route to process the delete request
+router.post("/delete", async (req, res, next) => {
+  try {
+    // Call controller function to handle the actual deletion
+    await invController.deleteInventoryItem(req, res, next)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
