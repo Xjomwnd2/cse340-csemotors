cac65c9923e592036feaa4cc6c9aef280a2ed2b7
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-06 22:53:09 +0000

    Jun 7, 2025, 1:53 AM

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,8 +3,6 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
-const regValidate = require("../utilities/account-validation");
-
 
 // Only require validation if the file exists
 let regValidate;
@@ -15,11 +13,9 @@ try {
   regValidate = null;
 }
 
-// Route to build "My Account" view - COMMENTED OUT until you implement buildAccount
-// router.get(
-//   "/", 
-//   utilities.handleErrors(accountController.buildAccount)
-// );
+// Route to build "My Account" view
+// Uncomment and implement this later
+// router.get("/", utilities.handleErrors(accountController.buildAccount));
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -27,26 +23,34 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to deliver the registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// Display the registration form
-router.get('/register', accountController.buildRegister);
+// Handle POST routes based on validation availability
+if (regValidate) {
+  // Process the registration data
+  router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  );
 
-// Route to process the registration form submission
-router.post('/register', utilities.handleErrors(accountController.register));
+  // Process the login request
+  router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    utilities.handleErrors(accountController.accountLogin)
+  );
+} else {
+  // If validation is missing, fallback to minimal handlers
+  router.post(
+    "/register",
+    utilities.handleErrors(accountController.registerAccount)
+  );
 
-// Process the registration data
-router.post(
-  "/register",
-  regValidate.registationRules(),
-  regValidate.checkRegData,
-  utilities.handleErrors(accountController.registerAccount)
-)
-
-// Process the login request
-router.post(
-  "/login",
-  regValidate.loginRules(),
-  regValidate.checkLoginData,
-  utilities.handleErrors(accountController.accountLogin)
-);
+  router.post(
+    "/login",
+    utilities.handleErrors(accountController.accountLogin)
+  );
+}
 
 module.exports = router;
