e781f3c61ea5f67d55d336432cc6a6da2173fc1a
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-06 05:19:49 +0300

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
-const regValidate = require('../utilities/account-validation');
 
 // Only require validation if the file exists
 let regValidate;
