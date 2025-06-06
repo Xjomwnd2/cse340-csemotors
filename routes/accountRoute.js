52c0f802e8bf95e16f8a3490e1cc87685f6de4ec
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-06 23:05:07 +0000

    COMMIT

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,16 +1,16 @@
 // routes/accountRoute.js
 
-const express = require("express")
-const router = new express.Router() 
-const accountController = require("../controllers/accountController")
-const utilities = require("../utilities")
-const regValidate = require('../utilities/account-validation')
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation');
 
 // Route to build login view
-router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 // Route to build registration view
-router.get("/register", utilities.handleErrors(accountController.buildRegister))
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Process the registration data
 router.post(
@@ -18,7 +18,7 @@ router.post(
   regValidate.registrationRules(),
   regValidate.checkRegData,
   utilities.handleErrors(accountController.registerAccount)
-)
+);
 
 // Process the login attempt
 router.post(
@@ -26,22 +26,22 @@ router.post(
   regValidate.loginRules(),
   regValidate.checkLoginData,
   utilities.handleErrors(accountController.accountLogin)
-)
+);
 
 // Route to build account management view (requires login)
 router.get("/", 
   utilities.checkLogin,
   utilities.handleErrors(accountController.buildAccountManagement)
-)
+);
 
 // Route to logout
-router.get("/logout", utilities.handleErrors(accountController.accountLogout))
+router.get("/logout", utilities.handleErrors(accountController.accountLogout));
 
 // Route to build update account view (requires login)
 router.get("/update/:accountId", 
   utilities.checkLogin,
   utilities.handleErrors(accountController.buildAccountUpdate)
-)
+);
 
 // Process account information update
 router.post(
@@ -50,7 +50,7 @@ router.post(
   regValidate.updateAccountRules(),
   regValidate.checkUpdateData,
   utilities.handleErrors(accountController.updateAccount)
-)
+);
 
 // Process password change
 router.post(
@@ -59,6 +59,6 @@ router.post(
   regValidate.passwordRules(),
   regValidate.checkPasswordData,
   utilities.handleErrors(accountController.updatePassword)
-)
+);
 
-module.exports = router
+module.exports = router;
