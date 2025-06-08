6f6fec8351bc68fb17705c3becd81a9009e04a6b
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-09 01:17:04 +0300

    COMMIT

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -1,58 +1,27 @@
-const pool = require("../database/")
+const pool = require("../database/");
 
-/* ***************************
- *  Get all comments for a specific vehicle
- * ************************** */
-async function getCommentsByInventoryId(inv_id) {
+/* Insert new comment */
+async function addComment(invId, commenterName, commentText) {
   try {
-    const sql = `
-      SELECT comment_id, inv_id, comment_name, comment_text, comment_date 
-      FROM comments 
-      WHERE inv_id = $1 
-      ORDER BY comment_date DESC
-    `
-    const data = await pool.query(sql, [inv_id])
-    return data.rows
+    const sql = `INSERT INTO comments (inv_id, commenter_name, comment_text)
+                 VALUES ($1, $2, $3)`;
+    await pool.query(sql, [invId, commenterName, commentText]);
+    return true;
   } catch (error) {
-    console.error("getCommentsByInventoryId error " + error)
-    throw error
+    throw new Error("Database insert error: " + error.message);
   }
 }
 
-/* ***************************
- *  Add a new comment
- * ************************** */
-async function addComment(inv_id, comment_name, comment_text) {
+/* Get comments for a specific vehicle */
+async function getCommentsByInvId(invId) {
   try {
-    const sql = `
-      INSERT INTO comments (inv_id, comment_name, comment_text) 
-      VALUES ($1, $2, $3) 
-      RETURNING *
-    `
-    const data = await pool.query(sql, [inv_id, comment_name, comment_text])
-    return data.rows[0]
+    const sql = `SELECT commenter_name, comment_text, comment_date
+                 FROM comments WHERE inv_id = $1 ORDER BY comment_date DESC`;
+    const result = await pool.query(sql, [invId]);
+    return result.rows;
   } catch (error) {
-    console.error("addComment error " + error)
-    throw error
+    throw new Error("Database query error: " + error.message);
   }
 }
 
-/* ***************************
- *  Delete a comment (optional for admin features)
- * ************************** */
-async function deleteComment(comment_id) {
-  try {
-    const sql = "DELETE FROM comments WHERE comment_id = $1"
-    const data = await pool.query(sql, [comment_id])
-    return data.rowCount > 0
-  } catch (error) {
-    console.error("deleteComment error " + error)
-    throw error
-  }
-}
-
-module.exports = {
-  getCommentsByInventoryId,
-  addComment,
-  deleteComment
-}
+module.exports = { addComment, getCommentsByInvId };
