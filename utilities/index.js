3f30cbb2de0400b5a1d45cc97c9632f105f57afb
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-09 02:28:47 +0300

    COMMIT

--- utilities/index.js
+++ utilities/index.js
@@ -1,19 +1,15 @@
 const invModel = require("../models/inventory-model");
 const Util = {};
-// Just delete or comment out those lines
-// const data = require('./someFile');
-// console.log(data);
 
 /* ************************
  * Constructs the nav HTML unordered list
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications();
-  console.log(data);
-  let list = "<ul>"
-  list += '<li><a href="/" title="Home page">Home</a></li>'
+  let list = "<ul>";
+  list += '<li><a href="/" title="Home page">Home</a></li>';
   data.rows.forEach((row) => {
-    list += "<li>"
+    list += "<li>";
     list +=
       '<a href="/inv/type/' +
       row.classification_id +
@@ -21,66 +17,136 @@ Util.getNav = async function (req, res, next) {
       row.classification_name +
       ' vehicles">' +
       row.classification_name +
-      "</a>"
-    list += "</li>"
+      "</a>";
+    list += "</li>";
   });
-  list += "</ul>"
+  list += "</ul>";
   return list;
 };
 
 /* **************************************
-* Build the classification view HTML
-* ************************************ */
-Util.buildClassificationGrid = async function(data){
+ * Build the classification view HTML
+ *************************************** */
+Util.buildClassificationGrid = async function (data) {
   let grid;
-  if(data.length > 0){
-    grid = '<ul id="inv-display">'
-    data.forEach(vehicle => { 
+  if (data.length > 0) {
+    grid = '<ul id="inv-display">';
+    data.forEach((vehicle) => {
       grid += '<li>';
-      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>';
+      grid +=
+        '<a href="../../inv/detail/' +
+        vehicle.inv_id +
+        '" title="View ' +
+        vehicle.inv_make +
+        ' ' +
+        vehicle.inv_model +
+        ' details"><img src="' +
+        vehicle.inv_thumbnail +
+        '" alt="Image of ' +
+        vehicle.inv_make +
+        ' ' +
+        vehicle.inv_model +
+        ' on CSE Motors" /></a>';
       grid += '<div class="namePrice">';
       grid += '<hr />';
       grid += '<h2>';
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>';
+      grid +=
+        '<a href="../../inv/detail/' +
+        vehicle.inv_id +
+        '" title="View ' +
+        vehicle.inv_make +
+        ' ' +
+        vehicle.inv_model +
+        ' details">' +
+        vehicle.inv_make +
+        ' ' +
+        vehicle.inv_model +
+        '</a>';
       grid += '</h2>';
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
+      grid +=
+        '<span>$' +
+        new Intl.NumberFormat("en-US").format(vehicle.inv_price) +
+        '</span>';
       grid += '</div>';
       grid += '</li>';
     });
     grid += '</ul>';
-  } else { 
-    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>';
+  } else {
+    grid =
+      '<p class="notice">Sorry, no matching vehicles could be found.</p>';
   }
   return grid;
 };
 
+/* **************************************
+ * Build the vehicle detail view HTML
+ *************************************** */
+Util.buildVehicleDetailHTML = async function (vehicle) {
+  if (vehicle) {
+    let detailHTML = '<div class="vehicle-detail">';
+    detailHTML += '<div class="vehicle-image">';
+    detailHTML +=
+      '<img src="' +
+      vehicle.inv_image +
+      '" alt="' +
+      vehicle.inv_make +
+      ' ' +
+      vehicle.inv_model +
+      '">';
+    detailHTML += '</div>';
+    detailHTML += '<div class="vehicle-info">';
+    detailHTML +=
+      '<h2>' + vehicle.inv_make + ' ' + vehicle.inv_model + ' Details</h2>';
+    detailHTML +=
+      '<p class="price"><strong>Price: $' +
+      new Intl.NumberFormat("en-US").format(vehicle.inv_price) +
+      '</strong></p>';
+    detailHTML +=
+      '<p><strong>Description:</strong> ' +
+      vehicle.inv_description +
+      '</p>';
+    detailHTML +=
+      '<p><strong>Color:</strong> ' + vehicle.inv_color + '</p>';
+    detailHTML +=
+      '<p><strong>Miles:</strong> ' +
+      new Intl.NumberFormat("en-US").format(vehicle.inv_miles) +
+      '</p>';
+    detailHTML += '</div>';
+    detailHTML += '</div>';
+    return detailHTML;
+  }
+};
+
+/* ************************
+ * Build classification list for dropdown
+ ************************** */
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications();
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>';
+  classificationList +=
+    "<option value=''>Choose a Classification</option>";
+  data.rows.forEach((row) => {
+    classificationList +=
+      '<option value="' + row.classification_id + '"';
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected ";
+    }
+    classificationList += ">" + row.classification_name + "</option>";
+  });
+  classificationList += "</select>";
+  return classificationList;
+};
 
 /* ****************************************
  * Middleware For Handling Errors
  * Wrap other function in this for 
  * General Error Handling
  **************************************** */
-Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-
-
-/* ****************************************
- *  Check Login
- * ************************************ */
- Util.checkLogin = (req, res, next) => {
-  if (res.locals.loggedin) {
-    next()
-  } else {
-    req.flash("notice", "Please log in.")
-    return res.redirect("/account/login")
-  }
- };
-
+Util.handleErrors =
+  (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
 module.exports = Util;
