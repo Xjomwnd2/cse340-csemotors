d8cb39858d83b798d18bb31b5fffbc670ed955a1
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-21 20:26:26 +0000

    Jun 21, 2025, 11:26 PM

--- controllers/invController.js
+++ controllers/invController.js
@@ -270,5 +270,6 @@ module.exports = {
   buildVehicleDetail: invCont.buildVehicleDetail, 
   buildManagementView: invCont.buildManagementView,
   buildAddClassification,
+  buildByInvId
   // other functions... 
 };
