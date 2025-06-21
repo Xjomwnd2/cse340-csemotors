0dde2cc7796e6fca16bba31b9464517e808d3b15
YourGitHubUsername <jmwaura@byupathway.edu>
2025-06-22 01:45:11 +0300

    commit

--- routes/commentsRoute.js
+++ routes/commentsRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const commentsController = require("../controllers/commentController");
+const commentsController = require("../controllers/commentsController");
 
 router.post("/add/:invId", commentsController.postComment);
 
